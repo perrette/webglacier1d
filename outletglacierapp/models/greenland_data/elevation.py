7414bf70ac733047739b9b0a47a80174127e214b
perrette <mahe.perrette@pik-potsdam.de>
2015-04-11 00:05:15 +0200

    maxshape now operate before loading netCDF

--- outletglacierapp/models/greenland_data/elevation.py
+++ outletglacierapp/models/greenland_data/elevation.py
@@ -16,6 +16,32 @@ from . import outlet_glacier_region as region
 DATASET = 'bamber2013'
 #DATASET = 'bamber2001'
 
+def _transform_bbox(bbox, crs1, crs2):
+    llx, lly, urx, ury = bbox
+    llx, lly = crs2.transform_point(llx, lly, crs1)
+    urx, ury = crs2.transform_point(urx, ury, crs1)
+    # to also compute other corners (because of rotations)
+    lrx, lry = crs2.transform_point(urx, lly, crs1)
+    ulx, uly = crs2.transform_point(llx, ury, crs1)
+    urx = max([urx, lrx]) 
+    ury = max([uly, ury]) 
+    llx = min([llx, ulx]) - 100e3 # fix to rignot_mouginot2012
+    lly = min([lly, lry]) 
+    bbox2 = llx, lly, urx, ury
+    return bbox2
+
+
+def _check_subsampling(ni, nj, maxshape):
+    # sub-sampling the data?
+    maxi, maxj = maxshape
+    si = np.floor_divide(ni, maxi)
+    sj = np.floor_divide(nj, maxj)
+    si = max([1, si])
+    sj = max([1, sj])
+    if si!=1 or sj!=1:
+        print 'subsampling before coord transform (elevation): ',si,sj
+    return si, sj
+
 def load(bbox=None, dataset=None, crs=None, variable=None, maxshape=None):
     """ Load elevation data
 
@@ -78,20 +104,6 @@ def load(bbox=None, dataset=None, crs=None, variable=None, maxshape=None):
         else:
             raise Exception('Unknown dataset '+dataset)
 
-        def transform_bbox(bbox, crs1, crs2):
-            llx, lly, urx, ury = bbox
-            llx, lly = crs2.transform_point(llx, lly, crs1)
-            urx, ury = crs2.transform_point(urx, ury, crs1)
-            # to also compute other corners (because of rotations)
-            lrx, lry = crs2.transform_point(urx, lly, crs1)
-            ulx, uly = crs2.transform_point(llx, ury, crs1)
-            urx = max([urx, lrx]) 
-            ury = max([uly, ury]) 
-            llx = min([llx, ulx]) - 100e3 # fix to rignot_mouginot2012
-            lly = min([lly, lry]) 
-            bbox2 = llx, lly, urx, ury
-            return bbox2
-
         ds = da.open_nc(ncfile)
 
         # get actual dimension names (e.g. Bamber, variable name and 
@@ -99,23 +111,36 @@ def load(bbox=None, dataset=None, crs=None, variable=None, maxshape=None):
         xdim = ds.nc.variables[xn].dimensions[0] 
         ydim = ds.nc.variables[yn].dimensions[0] 
 
+        # check size
+
         # Determine a region to load
         if bbox is not None:
             if dataset == 'morlighem2014':
                 bamber_crs = get_crs(bamber2013.MAPPING) # original mapping
                 data_crs = get_crs(mapping) # original mapping
-                bbox = transform_bbox(bbox, bamber_crs, data_crs)
+                bbox = _transform_bbox(bbox, bamber_crs, data_crs)
             # default CRS is OK
             # data_crs = get_crs(mapping) # original mapping
             llx, lly, urx, ury = bbox
+            # load axes
             x = ds.nc.variables[xn][:]
             y = ds.nc.variables[yn][:]
-            ind_i = (y >= lly) & (y <= ury)
-            ind_j = (x >= llx) & (x <= urx)
+            ind_i = np.where((y >= lly) & (y <= ury))[0]
+            ind_j = np.where((x >= llx) & (x <= urx))[0]
+            if maxshape is not None:
+                si, sj = _check_subsampling(ind_i.size, ind_j.size, maxshape)
+                ind_i = ind_i[::si]
+                ind_j = ind_j[::sj]
             ix = {ydim:ind_i,xdim:ind_j}
 
         else:
-            ix = {ydim:slice(None),xdim:slice(None)}
+            if maxshape is not None:
+                ni, nj = ds.nc.variables[yn].size, ds.nc.variables[xn].size
+                si, sj = _check_subsampling(ni, nj, maxshape)
+                ix = {ydim:slice(None,None,si),xdim:slice(None,None,sj)}
+            else:
+                ix = {ydim:slice(None),xdim:slice(None)}
+
 
         if dataset == 'bamber2001':
             ix['time'] = 0
@@ -147,27 +172,12 @@ def load(bbox=None, dataset=None, crs=None, variable=None, maxshape=None):
         # assert ds.dims == (yn, xn), repr((ds.dims, yn, xn))
         ds.dims = ('y','x')
 
-        # Transform to the appropriate coordinate system
-        # this is the time-intensive part, so better subsample here...
-        if maxshape is not None:
-            maxi, maxj = maxshape
-            ny, nx = ds.y.size, ds.x.size
-            si = np.floor_divide(ny, maxi)
-            sj = np.floor_divide(nx, maxj)
-            si = max([1, si])
-            sj = max([1, sj])
-            if si != 1 or sj != 1:
-                print 'subsampling before coord transform (elevation): ',si,sj
-                datasets_sub = [(k, ds[k].ix[::si, ::sj]) for k in ds.keys()]
-                ds = da.Dataset(datasets_sub)
-                # j = j[::sj]
-                # i = i[::si]
-
         # just indicate the provenance
         ds.dataset = dataset
         ds.ncfile = ncfile
 
 
+    # Transform to the appropriate coordinate system
     orig_crs = get_crs(mapping) # original projection system
     if crs is not None and crs.proj4_init != orig_crs.proj4_init:
         ds2 = da.Dataset()
